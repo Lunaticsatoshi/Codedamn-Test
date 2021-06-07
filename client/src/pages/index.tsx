6a1362375a8083e7d10042b5bbecc2da614aa385
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-07 19:47:00 +0530

    :sparkles: changing routes

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import dynamic from "next/dynamic";
 
+const CodeEditor = dynamic(() => import("../components/CodeEditor"), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello</h1>
+      <CodeEditor />
     </div>
-  )
+  );
 }
