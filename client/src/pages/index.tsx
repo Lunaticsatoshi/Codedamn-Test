5b9d0b70d05e5ef03dcdda235867d08d58a8b42c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-09 18:04:47 +0530

    :sparkles: Adding Navbar component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,21 +4,31 @@ import styles from "../../styles/Home.module.css";
 import dynamic from "next/dynamic";
 
 import SplitPane from "react-split-pane";
-import Pane from "react-split-pane";
 
 const CodeEditor = dynamic(() => import("../components/CodeEditor"), {
   ssr: false,
 });
+
+const Navbar = dynamic(() => import("../components/Navbar"), {
+  ssr: false,
+});
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <SplitPane split="vertical" minSize={50}>
-        <CodeEditor />
-        <SplitPane split="horizontal">
-          <h1>Right Top Pane</h1>
-          <h1>Right Bottom Pane</h1>
+    <div>
+      <Navbar />
+      <div className={styles.container}>
+        <SplitPane split="vertical" minSize={50}>
+          <CodeEditor />
+          <SplitPane split="horizontal">
+            <div className={styles.pane}>
+              <h1>Right Top Pane</h1>
+            </div>
+            <div className={styles.pane}>
+              <h1>Right Bottom Pane</h1>
+            </div>
+          </SplitPane>
         </SplitPane>
-      </SplitPane>
+      </div>
     </div>
   );
 }
