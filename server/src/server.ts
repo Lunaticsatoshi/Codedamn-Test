5ed9e926851d955df069ed3106157f59dbdc4347
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-11 16:12:57 +0530

    :sparkles: creating server

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Application, json } from "express";
+import cors from "cors";
+// import {connectDB} from "./config/db";
+
+const app: Application = express();
+
+app.use(cors());
+
+app.use(json());
+
+// app.use(todoRoutes);
+
+// connectDB();
+
+const PORT: string | number = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
