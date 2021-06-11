c704dee7d58245e3eec40df9ea32181f5c188244
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-11 16:21:56 +0530

    :card_file_box: connecting to mongo db database

---
+++ server/src/config/db.ts
@@ -0,0 +1,21 @@
+import { connect } from "mongoose";
+import dotenv from "dotenv";
+
+// dotenv.config({ path: "src/config/.env" });
+
+const mongoURI: string = process.env.MONGOURI!;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+export const connectDB = async () => {
+  try {
+    await connect(mongoURI, options);
+    console.log("Connected to MongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
