031119a7a6c8135d33b76817b130d3d8ba77656c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-06-12 19:38:30 +0530

    :package: adding language selector

--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -1,38 +1,19 @@
-import React, { useState, useEffect, useRef, FC } from "react";
+import React, { useRef } from "react";
 import Editor from "@monaco-editor/react";
-import axios from "axios";
 
-const CodeEditor: FC = (): JSX.Element => {
-  const [language, setLanguage] = useState("javascript");
-  const [value, setValue] = useState("//comment");
+type props = {
+  language: string;
+  value: string;
+};
+
+const CodeEditor = (props: props) => {
   const editorRef = useRef(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const { language } = await getData();
-      const { file, description } = language[0];
-      setLanguage(file);
-      setValue(description);
-      console.log(React.createElement("span", null));
-    };
-
-    fetchData();
-  }, []);
-
-  window.onload = () => {
-    alert("loaded");
-  };
-
-  const getData = async () => {
-    const { data } = await axios.get("http://localhost:5000/api/v1/languages");
-    return data;
-  };
   const handleEditorDidMount = (editor, monaco) => {
     editorRef.current = editor;
   };
 
   const handleEditorChange = (value, event) => {
     console.log("here is the current model value:", value);
-    setValue(value);
   };
 
   const handleEditorValidation = (markers) => {
@@ -40,10 +21,14 @@ const CodeEditor: FC = (): JSX.Element => {
     markers.forEach((marker) => console.log("onValidate:", marker.message));
   };
 
+  window.onload = function () {
+    alert("reload");
+  };
+
   return (
     <Editor
-      defaultLanguage={language}
-      defaultValue={value}
+      defaultLanguage={props.language}
+      defaultValue={props.value}
       onMount={handleEditorDidMount}
       onChange={handleEditorChange}
       onValidate={handleEditorValidation}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/Home.module.css";
 import dynamic from "next/dynamic";
+import axios from "axios";
 
 import SplitPane from "react-split-pane";
 
@@ -16,12 +16,30 @@ const Navbar = dynamic(() => import("../components/Navbar"), {
 const DynamicTerminal = dynamic(() => import("../components/Terminal"), {
   ssr: false,
 });
+
 export default function Home() {
+  const [language, setLanguage] = useState("javascript");
+  const [value, setValue] = useState("//comment");
+  useEffect(() => {
+    const fetchData = async () => {
+      const { language } = await getData();
+      const { file, description } = language[0];
+      setLanguage(file);
+      setValue(description);
+      console.log(React.createElement("span", null));
+    };
+    fetchData();
+  }, []);
+
+  const getData = async () => {
+    const { data } = await axios.get("http://localhost:5000/api/v1/languages");
+    return data;
+  };
   return (
     <div>
       <Navbar />
       <div className="select__inputs">
-        <select className="select__input" id="Language">
+        <select className="select__input" id="language">
           <option>javascript</option>
           <option>javascript</option>
           <option>javascript</option>
@@ -30,7 +48,7 @@ export default function Home() {
       </div>
       <div className={styles.container}>
         <SplitPane split="vertical" minSize={50}>
-          <CodeEditor />
+          <CodeEditor language={language} value={value} />
           <SplitPane split="horizontal">
             <div className={styles.pane}>
               <h1>Right Top Pane</h1>
